8a7310cfda47719596d8df67df4eb49b9b64484c
Teague Cole <tnc0419@icloud.com>
2022-03-06 22:20:15 -0500

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Teague Cole on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
